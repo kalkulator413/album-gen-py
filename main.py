3894adfc4a2fa09868b93befe26c730f606866f3
Kalkin <50224596+kalkulator413@users.noreply.github.com>
2022-08-14 01:05:43 -0700

    added reccomdation feature

--- main.py
+++ main.py
@@ -3,44 +3,81 @@ import random
 import PySimpleGUI as sg
 import webbrowser
 from spotipy.oauth2 import SpotifyPKCE
+import spotipy
+import os
+import pickle
 
 def main():
     #authenticate spotify credentials
-    auth_manager=SpotifyPKCE(client_id = '733feec74613475496335bd86b89e056', redirect_uri='http://localhost:8888')
+    auth_manager=SpotifyPKCE(client_id = '733feec74613475496335bd86b89e056',
+        scope = 'user-top-read', redirect_uri='http://localhost:8888')
     cache_token = auth_manager.get_access_token()
     
     sg.theme('DarkAmber')   # Add a touch of color
     # All the stuff inside your window.
     layout = [  [sg.Text('ENTER A GENRE ("any" and "new" are valid entries)')],
                 [sg.InputText()],
-                [sg.Button('Ok'), sg.Button('Cancel')] ]
+                [sg.Button('Ok'), sg.Button('Cancel'), sg.Button('Recommend something')] ]
 
     # Create the Window
     window = sg.Window('Choose albums', layout)
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
+        genre = ''
         if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
             break
-
-        # get genre from user
-        genre = values[0]
-
-        # get a list of albums
-        albums = get_albums(genre, cache_token)
-        albums_copy = albums[:]
-
-        chosen_albums = []
-        # take 9 random albums
-        for x in range(1, 10):
-            album = random.choice(albums_copy)
-            albums_copy.remove(album)
-            chosen_albums.append(album)
-            # print(str(x) + '.', album, '\n')
-
-        make_new_window(chosen_albums, albums)
+        if event == 'Recommend something':
+            genre = get_user_top_genre(cache_token)
+        if event == 'Ok':
+            genre = values[0]
+        make_window(genre, cache_token)        
 
     window.close()
+
+def make_window(genre, cache_token):
+    # get a list of albums
+    albums = get_albums(genre, cache_token)
+    albums_copy = albums[:]
+
+    chosen_albums = []
+    # take 9 random albums
+    for x in range(1, 10):
+        album = random.choice(albums_copy)
+        albums_copy.remove(album)
+        chosen_albums.append(album)
+        # print(str(x) + '.', album, '\n')
+
+    make_new_window(chosen_albums, albums)
+
+def get_user_top_genre(cache_token):
+    sp = spotipy.Spotify(cache_token)
+    artists = sp.current_user_top_artists()['items']
+
+    gFile = os.path.join("genreSet")
+    infile = open(gFile,'rb')
+    gSet = pickle.load(infile)
+    infile.close()
+
+    user_genres = set()
+
+    for artist in artists:
+        if 'genres' not in artist:
+            continue
+        g_list = artist['genres']
+        for genre in g_list:
+            user_genres.add(genre)
+
+    chosen_genre = ''
+    while chosen_genre.replace(' ', '-').lower() not in gSet and user_genres:
+        chosen_genre = random.choice(tuple(user_genres))
+        user_genres.remove(chosen_genre)
+
+    if chosen_genre.replace(' ', '-').lower() not in gSet and not user_genres:
+        return "any"
+
+    return chosen_genre
+
 
 def make_new_window(albums, full_list):
     grid = []
